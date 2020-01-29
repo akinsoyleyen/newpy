4c2d7c1d7a8bcdb181e1801f38b25148cfd16ed6
İhsan Akin Soyleyen <akinsoyleyen>
2020-01-29 20:52:29 +0300

    v1.4

--- abs_selenium.py
+++ abs_selenium.py
@@ -1,48 +1,52 @@
 from selenium import webdriver
 import time
 
+containerNumbers = ['GESU9385802', 'HLBU9369360']
+
 #!HAPAGLLOYD CLASS
 
 class hapagContainerTracking:
 
     def __init__(self, shippingLine, containerNumber):
+        
+            print('Now looking for' + ' ' + containerNumber)
 
-        self.shippingLine = shippingLine
-        self.containerNumber = containerNumber
+            self.shippingLine = shippingLine
+            self.containerNumber = containerNumber
 
-        global hapagContainerType
-        global hapagLastMovement
+            global hapagContainerType
+            global hapagLastMovement
 
-        browser = webdriver.Chrome()
-        browser.get(shippingLine)
+            browser = webdriver.Chrome()
+            browser.get(shippingLine)
 
-        # Arama alaninin bulunmasi
-        searchField = browser.find_element_by_css_selector('#tracingvalue')
-        time.sleep(7)
+            # Arama alaninin bulunmasi
+            searchField = browser.find_element_by_css_selector('#tracingvalue')
+            time.sleep(7)
 
-        # Arama alanina konteyner numarasinin yazilmasi
-        searchFieldFilled = searchField.send_keys(containerNumber)
+            # Arama alanina konteyner numarasinin yazilmasi
+            searchFieldFilled = searchField.send_keys(containerNumber)
 
-        # Trace Button basma
-        hapagButton = browser.find_element_by_css_selector(
-            'body > div.hal-page.hal-page--home.page-home > div.hal-page-body > div.hal-stagehome > div:nth-child(3) > div > div > div:nth-child(1) > div > button > span')
-        hapagButton.click()
+            # Trace Button basma
+            hapagButton = browser.find_element_by_css_selector(
+                'body > div.hal-page.hal-page--home.page-home > div.hal-page-body > div.hal-stagehome > div:nth-child(3) > div > div > div:nth-child(1) > div > button > span')
+            hapagButton.click()
 
-        # 5sn bekleme
-        time.sleep(5)
+            # 5sn bekleme
+            time.sleep(5)
 
-        # Container Type olan alanin bulunmasi
-        hapagContainerType = browser.find_element_by_css_selector(
-            '#tracing_by_container_f\:hl29 > tbody > tr > td > div > table > tbody > tr > td:nth-child(3) > table > tbody > tr > td.inputNonEdit > span')
-        hapagLastMovement = browser.find_element_by_css_selector(
-            '#tracing_by_container_f\:hl56 > tbody > tr > td > div > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td.inputNonEdit > span')
+            # Container Type olan alanin bulunmasi
+            hapagContainerType = browser.find_element_by_css_selector(
+                '#tracing_by_container_f\:hl29 > tbody > tr > td > div > table > tbody > tr > td:nth-child(3) > table > tbody > tr > td.inputNonEdit > span')
+            hapagLastMovement = browser.find_element_by_css_selector(
+                '#tracing_by_container_f\:hl56 > tbody > tr > td > div > table > tbody > tr > td:nth-child(1) > table > tbody > tr > td.inputNonEdit > span')
 
-        # Container tipi nedir?
-        hapagContainerType = hapagContainerType.text
-        hapagLastMovement = hapagLastMovement.text
+            # Container tipi nedir?
+            hapagContainerType = hapagContainerType.text
+            hapagLastMovement = hapagLastMovement.text
 
-        print('The Type of the Container is:' + hapagContainerType)
-        print(hapagLastMovement)
+            print('The Type of the Container is:' + hapagContainerType)
+            print(hapagLastMovement)
 
 
 # Hangi shipping line kontrol edilecek?
@@ -51,9 +55,9 @@ shippingLine = input('What is the shipping line?').lower()
 #!THE IF STATEMENT PART OF THE PROGRAM
 if shippingLine == "hapaglloyd":
     shippingLine = 'https://www.hapag-lloyd.com/en/home.html'
-    containerNumber = input('What is the container number?')
     print("OK....Passing to the relative class")
-    hapagContainerTracking(shippingLine, containerNumber)
+    for containerNumber in containerNumbers:
+        hapagContainerTracking(shippingLine, containerNumber)
 else:
     print('We only support HapagLloyd for now!')
 
@@ -61,3 +65,5 @@ else:
 
 containerStatusFile = open("Container.txt", "w")
 containerStatusFile.write(containerNumber + " " + hapagLastMovement)
+
+#! containerNumbers = ['GESU9385802', 'HLBU9369360']
---
+++ containers.py
@@ -0,0 +1,4 @@
+containerNumbers = ['GESU9385802', 'HLBU9369360']
+
+for containerNumber in containerNumbers:
+    print(containerNumber)
