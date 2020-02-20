31122dc83ab6f4028ba74762fcea89d7013e554d
Ubuntu <akinsoyleyen@Python.32tudaffkduuvf00jz1fqkfind.parx.internal.cloudapp.net>
2020-02-20 06:44:24 +0000

    v23

--- currency_rates.py
+++ currency_rates.py
@@ -1,11 +1,19 @@
 from forex_python.converter import CurrencyRates   
+from forex_python.converter import CurrencyCodes
 
-currency = CurrencyRates()
+def usd_to_try():
+    
+    currency = CurrencyRates()
+    currency_symbol = CurrencyCodes().get_symbol('USD')
 
-usd_dict = currency.get_rates('USD')
 
-usd_to_try = usd_dict['TRY']
+    convert_amount = input('How much money would you like to convert?')
+    convert_amount = int(convert_amount)
 
+    usd_to_try_converter = currency.convert('USD', 'TRY', convert_amount )
 
+    print(currency_symbol + str(usd_to_try_converter))
+
+def usd_to_try()
 
 
